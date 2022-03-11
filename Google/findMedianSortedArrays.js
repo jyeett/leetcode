38125d35b7be43b9d277d4e718a97a6efd77d001
Jordan Yee <jordanyee799@gmail.com>
2022-03-11 00:48:32 -0800

    add details

--- Google/findMedianSortedArrays.js
+++ Google/findMedianSortedArrays.js
@@ -1,3 +1,8 @@
+// Given two sorted arrays nums1 and nums2 of size m and n respectively,
+// return the median of the two sorted arrays.
+
+// The overall run time complexity should be O(log (m+n)).
+
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
